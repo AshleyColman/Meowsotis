923653bcafe216673398e5d7fc4e07dc4f6b4676
Spaceman <spaceman.development@outlook.com>
2026-04-03 15:30:56 +0100

    Hover nav bar

---
+++ src/js/nav.js
@@ -0,0 +1,32 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+   
+    const navSwitch = document.querySelector('#nav-switch');
+    const header = document.querySelector('#header');
+    
+    console.log(navSwitch);
+    
+    if (!navSwitch || !header)
+        return;
+    
+    const fadeInClass = 'fadeIn';
+    const fadeOutClass = 'fadeOut';
+    
+    console.log('clicked');
+    
+    navSwitch.addEventListener('click', () => {
+        
+        if (!header.classList.contains(fadeInClass) && !header.classList.contains(fadeOutClass)) {
+            header.classList.add(fadeInClass);
+            return;
+        }
+        
+        if (header.classList.contains(fadeInClass)) {
+            header.classList.remove(fadeInClass);
+            header.classList.add(fadeOutClass);
+        }
+        else if (header.classList.contains(fadeOutClass)) {
+            header.classList.remove(fadeOutClass);
+            header.classList.add(fadeInClass);
+        }
+    });
+});
