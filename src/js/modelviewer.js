1dba07d1ec9220594fae95f1b0e6b6dc7750ec02
Spaceman <spaceman.development@outlook.com>
2026-04-11 19:22:30 +0100

    Model view loading with js

--- src/js/modelviewer.js
+++ src/js/modelviewer.js
@@ -1,52 +1,46 @@
 ﻿document.addEventListener('DOMContentLoaded', () => {
-    
+
     const modelViewer = document.getElementById('modelviewer');
     const sketchfabIframe = document.getElementById('sketchfab-embed-iframe');
     const grid = document.getElementById('portfolio-grid');
     const gridItems = grid.getElementsByClassName('grid-item');
     const closeButton = document.getElementById('modelviewer-close-button');
     
-    if (modelViewer == null || 
-        sketchfabIframe == null ||
-        gridItems == null || gridItems.length <= 0
-        || closeButton == null)
-    {
-        console.log("1 is null")
+    if (modelViewer == null || sketchfabIframe == null || gridItems == null || gridItems.length <= 0 || closeButton == null) {
         return;
-        
     }
-    
-    // gridItems.forEach((el) => {
-    //     el.addEventListener('click', () => {
-    //        
-    //     });
-    // });
 
+    addGridItemClickEvents()
 
+    function addGridItemClickEvents() {
+        Array.from(gridItems).forEach((el) => {
+            el.addEventListener('click', () => {
+                const sketchfabId = el.getAttribute('data-sketchfabid');
 
+                if (sketchfabId == null) return;
 
-
-
-
-
-
-
+                fadeInModelViewer(sketchfabId);
+            });
+        });
+    }
 
     const fadeInClass = 'fadeIn';
     const fadeOutClass = 'fadeOut';
+    
+    function fadeInModelViewer(sketchfabId) {
+        modelViewer.style.display = 'flex';
+        modelViewer.classList.remove(fadeOutClass);
+        modelViewer.classList.add(fadeInClass);
+
+        const sketchFabModelUrl = `https://sketchfab.com/models/${sketchfabId}/embed?ui_theme=dark&dnt=1&transparent=1&autospin=1&autostart=1`;
+
+        sketchfabIframe.setAttribute('src', sketchFabModelUrl);
+    }
 
     closeButton.addEventListener('click', () => {
-        const isHidden = getComputedStyle(modelViewer).display === 'none';
-
-        if (isHidden) {
-            modelViewer.style.display = 'block';
-            modelViewer.classList.remove(fadeOutClass);
-            modelViewer.classList.add(fadeInClass);
-        } else {
-            sketchfabIframe.removeAttribute('src');
-            modelViewer.classList.remove(fadeInClass);
-            modelViewer.classList.add(fadeOutClass);
-        }
+        sketchfabIframe.removeAttribute('src');
+        modelViewer.classList.remove(fadeInClass);
+        modelViewer.classList.add(fadeOutClass);
     });
 
     modelViewer.addEventListener('animationend', (event) => {
