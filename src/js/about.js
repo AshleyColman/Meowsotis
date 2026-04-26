c643c51317a6909c25aba1cd876c90538ee497f0
Spaceman <spaceman.development@outlook.com>
2026-04-26 17:39:32 +0100

    About me wip

---
+++ src/js/about.js
@@ -0,0 +1,20 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const translateTextButtons = document.getElementsByClassName('translate-text-button');
+    const englishText = document.getElementById('english-text');
+    const japaneseText = document.getElementById('japanese-text');
+
+    if (!translateTextButtons || !englishText || !japaneseText) {
+        return;
+    }
+
+    let isJapanese = false;
+
+    Array.from(translateTextButtons).forEach((el) => {
+        el.addEventListener('click', () => {
+            isJapanese = !isJapanese;
+            japaneseText.style.display = isJapanese ? 'flex' : 'none';
+            englishText.style.display = isJapanese ? 'none' : 'flex';
+        });
+    });
+
+});
