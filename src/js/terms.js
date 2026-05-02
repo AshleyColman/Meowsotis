d497e139906e7046f0e788f66a8cc2cde3c364a5
Spaceman <spaceman.development@outlook.com>
2026-05-02 15:27:49 +0100

    Terms translate

---
+++ src/js/terms.js
@@ -0,0 +1,26 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const translateTextButtons = document.getElementsByClassName('translate-text-button');
+
+    const englishTextElements = document.querySelectorAll('.english-text');
+    const japaneseTextElements = document.querySelectorAll('.japanese-text');
+
+    if (!translateTextButtons || !englishTextElements || !japaneseTextElements) {
+        return;
+    }
+
+    let isJapanese = false;
+
+    Array.from(translateTextButtons).forEach((button) => {
+        button.addEventListener('click', () => {
+            isJapanese = !isJapanese;
+
+            japaneseTextElements.forEach((element) => {
+                element.style.display = isJapanese ? 'block' : 'none';
+            });
+
+            englishTextElements.forEach((element) => {
+                element.style.display = isJapanese ? 'none' : 'block';
+            });
+        });
+    });
+});
